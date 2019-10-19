0bd2a234e4adda102cc275edb73b5d7ba0c35648
samuel sihombing <7316054@student.unpar.ac.id>
2019-10-19 11:50:21 +0700

    udah bisa baca Directory dan file

--- index.php
+++ index.php
@@ -7,6 +7,29 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+
+    $dir = opendir('DataSet');
+    while ($file = readdir($dir)) { //MEMBUKA DIRECTORY
+
+        if ($file == '.' || $file == '..') {
+            continue;
+        }
+
+         $currentFile=$file;
+         //untuk membaca file 
+         $fn = fopen("DataSet/".$currentFile,"r");
+         $document="";
+         while(! feof($fn))  {
+            $result = fgets($fn);
+         }
+         echo $result;
+
+         fclose($fn);
+        
+    }
+    closedir($dir);  //SELESAI MEMBACA SEMUA DIRECTORY
+?>
     
 </body>
 </html>
