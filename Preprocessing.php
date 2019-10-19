30a2e57e4a14034bc0cecc11379aa5541eb73aa5
muhammaddipo <muhammmaddipo@gmail.com>
2019-10-19 14:18:10 +0700

    nambah stemming

--- Preprocessing.php
+++ Preprocessing.php
@@ -1,27 +1,16 @@
 <?php
-    include 'vendor/autoload.php';
-
     class Preprocessing{
         private $stop_words;
-        private $stemming_list;
         public function __construct(){ 
             $this->stop_words=array
-            ("i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", 
-            "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",  "they", "them", "their", 
-            "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", 
-            "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", 
-            "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", 
-            "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in",  "out", "on", "off", 
-            "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how",  "all", "any", "both", 
-            "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same",  "so", "than", "too", 
-            "very", "s", "t", "can", "will", "just", "don", "should", "now");
+            ("i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",  "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in",  "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how",  "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same",  "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now");
         }
         
-        private function lowerCases($teks){
+        function lowerCases($teks){
             return strtolower(trim($teks));
         }
 
-        private function removePunctuation($teks){
+        function removePunctuation($teks){
             $teks = str_replace("'", " ", $teks);
             $teks = str_replace("-", " ", $teks);
             $teks = str_replace(")", " ", $teks);
@@ -38,19 +27,14 @@
             return $teks;
         }
 
-        private function removeStopWords($teks){
+        function removeStopWords($teks){
             return preg_replace('/\b('.implode('|',$this->stop_words).')\b/','',$teks);
         }
 
-        private function stemming($teks){
-            return \Nadar\Stemming\Stemm::stemPhrase($teks, 'en');
-        }
-
-        public function doPreprocessing($teks){
+        function doPreprocessing($teks){
             $result = $this->lowerCases($teks);
             $result = $this->removePunctuation($result);
             $result = $this->removeStopWords($result);
-            $result = $this->stemming($result);
             return $result;
         }
     }
