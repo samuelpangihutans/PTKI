1ff2eeaf022f2abf1d5c22696f4fc23831a6da70
samuel sihombing <7316054@student.unpar.ac.id>
2019-10-22 12:57:07 +0700

    tambah kelas statistik

---
+++ statistik.php
@@ -0,0 +1,38 @@
+<?php
+
+class statistik{
+    //Atribut isi disini PO !!
+
+
+    public function __construct(){ 
+    
+    }
+
+    public function jumlahDoc (){
+
+    }
+
+    public function jumlahWord (){
+        //Ganda dihitung , berarti diambil dari data mentah keseluruhan doc
+    }
+
+    public function jumlahTerm(){
+        //ganda g dihitung, berarti ambil dari data setelah celaning keseluruhan doc
+    }
+
+    public function jumlahRataRataWordDoc(){
+        
+    }
+
+    public function jumlahRataRataTermDoc(){
+      
+    }
+
+}
+
+
+
+
+
+
+?>
