333b5c6c03dc51b73640488f408a83b5af1a1e77
muhammaddipo <muhammmaddipo@gmail.com>
2019-11-06 07:06:15 +0700

    berusaha intersection

--- Search.php
+++ Search.php
@@ -8,18 +8,22 @@ class Search{
 
     public function search($input,$invertedIndex){
         $query=$this->clean->doPreprocessing($input);
-        $res=array();
+        $res=[];
+        $temp=[];
         $words=explode(" ",rtrim($query));
         for($i=0;$i<sizeof($words);$i++){
             if(array_key_exists($words[$i],$invertedIndex)==TRUE){
                 for($j=0;$j<sizeof((array)$invertedIndex[$words[$i]]);$j++){
                     // if(array_key_exists($words[$i],$invertedIndex)){
-                        echo $words[$i]." ".$invertedIndex[$words[$i]][$j]." ";
+                        $temp=$res[$invertedIndex[$words[$i]][$j]]*1;
+                        $res[$invertedIndex[$words[$i]][$j]]=$temp+1;
+                        
                     // }
                 }
                 echo "<br>";
             }
         }
+        print_r($res);
     }
 }
 
