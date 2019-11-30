d0eeb1e502716846934520da8d94fb2c46ea72f5
Anugrah Jaya <anugrahjaya1.bts.internship@gmail.com>
2019-11-30 13:11:03 +0700

    TF IDF sudah dihitung, belum dihitung jika ada query

--- TF_IDF.php
+++ TF_IDF.php
@@ -2,37 +2,32 @@
 
     class TF_IDF{
         private $keys;// untuk term tf_idf
-        private $result;
+        private $TF;
+        private $IDF;
+        private $TF_IDF;
         
 
         public function __construct(){
             $strJsonFileContents = file_get_contents("InvertedIdx/invertedIdx.json");
             $invertedIdx = json_decode($strJsonFileContents,true);
             $this->keys = array_keys($invertedIdx);
-            // foreach ($this->keys as $key){
-            //    print_r($key);
-            //    echo "<br>";         
-            // }
-            $this->result = [];
-            // print_r($this->keys);
+            $this->TF = [];
+            $this->IDF = [];
+            $this->TF_IDF = [];
         }
 
-        private function createTF(){
+        public function createTF(){
             $dir = opendir('DataCleaning');
             $idx=1;// untuk no dokumen
-            $temp = [];
             while ($file = readdir($dir)) { //MEMBUKA DIRECTORY
                 
                 if ($file == '.' || $file == '..') {
                     continue;
                 }
                     $currentFile=$file;
-                    //untuk membaca file 
-                    
-                    
+                    // looping per term di setiap dokumen
                     foreach ($this->keys as $key){
-                        // print($key);
-                        // echo "<br>";
+                        //untuk membaca file
                         $fn = fopen("DataCleaning/".$currentFile,"r");
                         $count = 0;
                         while(! feof($fn))  {
@@ -41,40 +36,108 @@
                             for($i = 0; $i<sizeof($split); $i++){
                                 // jika buka string kosong
                                 if($split[$i] != ""){
-                                    // print($split[$i]);
-                                    // echo "<br>";
-                                    if(strcmp($split[$i], $key) == 0){
+                                    if($split[$i] == $key){
                                         $count++;
-                                        // print("masuk");
                                     }
                                 }
                             }
                         }
-                        $temp[$idx]=$count;
-                        $this->result[$key]=$temp;
-                        // if($count !=0){
-                        //     print($count." ".$key);
-                        //     echo "<br>";
-                        // }
+                        $this->TF[$key][$idx]=$count;
                     }
                 $idx++;
                 fclose($fn);
             }
             closedir($dir);
         }
+
+        public function createTFQuery($query){
+            $dir = opendir('DataCleaning');
+            while ($file = readdir($dir)) { //MEMBUKA DIRECTORY
+                
+                if ($file == '.' || $file == '..') {
+                    continue;
+                }
+                    $currentFile=$file;
+                    // looping per term di setiap dokumen
+                    $query = explode(" ",$query);
+                    for($j = 0 ;$j<sizeof($query); $j++){
+                        //untuk membaca file
+                        $fn = fopen("DataCleaning/".$currentFile,"r");
+                        $count = 0;
+                        while(! feof($fn))  {
+                            $teks = fgets($fn);
+                            $split=explode(" ",$teks);
+                            for($i = 0; $i<sizeof($split); $i++){
+                                // jika buka string kosong
+                                if($split[$i] != ""){
+                                    if($split[$i] == $query[$j]){
+                                        $count++;
+                                    }
+                                }
+                            }
+                        }
+                        //idx 155 untuk array baru
+                        if(array_key_exists($query[$j],$this->keys)==TRUE){
+                            $this->TF[$key][155]=$count;
+                        }
+                        
+                    }
+                $idx++;
+                fclose($fn);
+            }
+            closedir($dir);
+        }
+
+        public function createIDF(){
+            foreach ($this->keys as $key){
+                $count = 0;
+                for($i = 1 ; $i<=154; $i++){
+                    if($this->TF[$key][$i]!=0){
+                        $count++;
+                    }
+                }
+                $this->IDF[$key] = log((154/$count),10);
+                // if($key=="fair"){
+                //     print($count);
+                //     print(log((154/$count),10));
+                // }
+            }
+        }
         
-        public function getResult(){
+        public function doTF_IDF(){
             $this->createTF();
-            print_r($this->result);
+            $this->createIDF();
+            $this->calcaulateTF_IDF();
+        }
+
+        public function calcaulateTF_IDF(){
+            foreach ($this->keys as $key){
+                for($i = 1 ;$i<154; $i++){
+                    $this->TF_IDF[$key][$i] = $this->TF[$key][$i]*$this->IDF[$key];
+                }
+            }
+        }
+
+        public function getTF(){
+            return $this->TF;
+        }
+
+        public function getIDF(){
+            return $this->IDF;
+        }
+        
+        public function getTF_IDF(){
+            print_r($this->TF_IDF);
         }
 
 
-        
+
     }
 
     
 
     $tf_idf = new TF_IDF();
-    $tf_idf->getResult();
+    $tf_idf->doTF_IDF();
+    $tf_idf->getTF_IDF();
 
 ?>
